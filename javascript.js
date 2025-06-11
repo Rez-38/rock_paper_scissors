9a97960c37630427d7a561cf71685dca5e63d314
Rez38 <136545384+Rez-38@users.noreply.github.com>
2025-06-10 18:48:32 -0700

    Move playRound(); into playGame(); and add handling in the event of a draw.
    playGame(); will call playRound(); and successfully cycle through 5
    rounds of rock, paper, scissors; while
    The result should be displayed after each individual round.
    
    if else statement will display the outcome of the match and
    will handle a tied outcome.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const paper = "Paper";
 const scissors = "Scissors";
 
 function getComputerChoice () {
-    
+    // return rock.toLowerCase(); useful during troubleshooting
     let choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     
     if (choice === 1) {
@@ -18,7 +18,6 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    
     let tool = prompt("Choose your tool...", "rock, paper, scissors");
     
     if (!tool) {
@@ -43,35 +42,67 @@ function getHumanChoice () {
     }
 }
 
-let humanScore = 0;
 let computerScore = 0;
+let humanScore = 0;
 
-function playRound (humanChoice, computerChoice) {
 
-    //if (humanChoice === !null) {
-    //    humanChoice = humanChoice.toLowerCase();
-    //}
-    humanChoice = humanChoice.toLowerCase();
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+// console.log(computerSelection); useful during troubleshooting
+// console.log(humanSelection); useful during troubleshooting
+
+
+function playGame () {
     
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        alert("You win this round! Rock beats Scissors!");
-        return ++humanScore;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        alert("You win this round! Paper beats Rock!");
-        return ++humanScore;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        alert("You win this round! Scissors beat Paper!");
-        return ++humanScore;
-    } else if (humanChoice === computerChoice) {
-        alert("We have a draw! You both chose the same tool!");
-        return null;
-    } else {
-        alert("You lose...");
-        return ++computerScore;
-    }    
+    function playRound (humanChoice, computerChoice) {
+        
+        //if (humanChoice === !null) {
+            //    humanChoice = humanChoice.toLowerCase();
+            //}
+        humanChoice = humanChoice.toLowerCase();
+            
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            alert("You win this round! Rock beats Scissors!");
+            return ++humanScore;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            alert("You win this round! Paper beats Rock!");
+            return ++humanScore;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            alert("You win this round! Scissors beat Paper!");
+            return ++humanScore;
+        } else if (humanChoice === computerChoice) {
+            alert("We have a draw! You both chose the same tool!");
+            return null;
+        } else {
+            alert("You lose...");
+            return ++computerScore;
+        }    
+    }
+
+        playRound(humanSelection, computerSelection);
+        console.log(computerScore);
+        console.log(humanScore);
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(computerScore);
+        console.log(humanScore);
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(computerScore);
+        console.log(humanScore);
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(computerScore);
+        console.log(humanScore);
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(computerScore);
+        console.log(humanScore);
+
+        if (computerScore === humanScore) {
+            alert("It's a DRAWWWW!!!!");
+        } else {
+            let winner = (computerScore < humanScore) ?
+                () => alert("Congratulations to the winner of this event!... Some random user!!!!!!!") :
+                () => alert("You have been defeated by the computer... The automatons have won! Skynet has crushed the human resistance!! Humanity could never regain control of Earth from the rogue AIs!!!!");
+            winner();
+        }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
