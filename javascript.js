7efd1a52fcb10e05101edb94803cbc4a96facba5
Rez38 <136545384+Rez-38@users.noreply.github.com>
2025-09-15 22:55:17 -0700

    Change winner, player & computer alerts and console.logs to be displayed on the user interface instead

--- javascript.js
+++ javascript.js
@@ -1,22 +1,35 @@
 console.log("Hello World!");
 
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
+// Creates a text log for all actions...
+const log = document.createElement('div');
+log.setAttribute('id', 'log');
+log.textContent = '';
+
+// Set up computer choice... (append computer to log)
+const computer = document.createElement('p');
+computer.setAttribute('id', 'computer');
+log.appendChild(computer);
 
 function getComputerChoice () {
     //// return rock.toLowerCase(); useful during troubleshooting
     let choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     
     if (choice === 1) {
-        console.log("Computer chose rock");
-        return rock.toLowerCase();
+        // console.log("Computer chose rock");
+        computer.textContent = 'Computer chose rock';
+        return rock;
     } else if (choice === 2) {
-        console.log("Computer chose paper");
-        return paper.toLowerCase();
+        // console.log("Computer chose paper");
+        computer.textContent = "Computer chose paper";
+        return paper;
     } else if (choice === 3) {
-        console.log("Computer chose scissors");
-        return scissors.toLowerCase();
+        // console.log("Computer chose scissors");
+        computer.textContent = 'Computer chose scissors';
+        return scissors;
     }
 }
 
@@ -25,28 +38,51 @@ alert("Make your choice... Best of Five will win!");
 let computerScore = 0;
 let humanScore = 0;
 
+
 function winner() {
     if (computerScore >= 5) {
-        alert("Computer Won!");
-        alert("Refresh the page to start again...");
+        winnerDisplay.textContent = 
+        "Computer Won! Refresh the page to start again...";
+        // alert("Computer Won!");
+        // alert("Refresh the page to start again...");
+        
     } else if (humanScore >= 5) {
-        alert("You Won!!!");
-        alert("Refresh the page to start over...");
+        winnerDisplay.textContent = 
+        "You Won!!! Refresh the page to start over...";
+        // alert("You Won!!!");
+        // alert("Refresh the page to start over...");
     } 
-
+    
     document.querySelector('#rockBtn').disabled = true;
     document.querySelector('#paperBtn').disabled = true;
     document.querySelector('#scissorsBtn').disabled = true;
 };
-    
-    
-    const scoreVal = document.querySelector('#scoreVal');
-const rockBtn = document.querySelector('#rockBtn');
-rockBtn.addEventListener('click', () => {
-    console.log("Rock was clicked");
-    let vsRock = getComputerChoice();
-    console.log(vsRock);
 
+// Placement... (insert log before scoreDiv )
+const body = document.body;
+const scoreDiv = document.querySelector("#score");
+body.insertBefore(log, scoreDiv);
+
+// Set up player choice... (Append player to log)
+const player = document.createElement('p');
+player.setAttribute('id', 'player');
+log.insertBefore(player, computer);
+
+// Display current scoress
+const scoreVal = document.querySelector('#scoreVal');
+const rockBtn = document.querySelector('#rockBtn');
+
+// Rock
+rockBtn.addEventListener('click', () => {
+    // console.log("Rock was clicked");
+    // Player action...
+    player.textContent = 'Player chose rock.';
+    
+    // Computer action...
+    let vsRock = getComputerChoice();
+    // console.log(vsRock);
+    
+    
     // Handle each scenario (Rock, paper, or scissors?)
     if (vsRock == "rock") {
         alert("Draw");
@@ -58,22 +94,26 @@ rockBtn.addEventListener('click', () => {
         alert("You win");
         ++humanScore;
     }
-
+    
     // Display Current Score
-    console.log("computer score: " + computerScore);
-    console.log("human score: " + humanScore);
+    // console.log("computer score: " + computerScore);
+    // console.log("human score: " + humanScore);
     scoreVal.textContent = `Computer Score: ${computerScore}. Your Score: ${humanScore}.`;
     if (computerScore < 5 && humanScore < 5) {
         return alert(`Computer Score: ${computerScore}. Your Score: ${humanScore}.`);
     } else { return winner();}
 });
 
+// Paper
 const paperBtn = document.querySelector('#paperBtn');
 paperBtn.addEventListener('click', () => {
-    console.log("Paper was clicked");
+    // console.log("Paper was clicked");
+    // Player action...
+    player.textContent = "Player chose paper.";
+    
     let vsPaper = getComputerChoice();
-    console.log(vsPaper);
-
+    // console.log(vsPaper);
+    
     // Handle each scenario
     if (vsPaper == "rock") {
         alert("You win");
@@ -85,22 +125,26 @@ paperBtn.addEventListener('click', () => {
         alert("You lose");
         ++computerScore;
     }
-
+    
     // Display Current Score
-    console.log("computer score: " + computerScore);
-    console.log("human score: " + humanScore);
+    // console.log("computer score: " + computerScore);
+    // console.log("human score: " + humanScore);
     scoreVal.textContent = `Computer Score: ${computerScore}. Your Score: ${humanScore}.`;
     if (computerScore < 5 && humanScore < 5) {
         return alert(`Computer Score: ${computerScore}. Your Score: ${humanScore}.`);
     } else { return winner();}
 });
 
+// Scissors
 const scissorsBtn = document.querySelector('#scissorsBtn');
 scissorsBtn.addEventListener('click', () => {
-    console.log("Scissors was clicked");
+    // console.log("Scissors was clicked");
+    // Player action...
+    player.textContent = "Player chose scissors.";
+    
     let vsScissors = getComputerChoice();
-    console.log(vsScissors);
-
+    // console.log(vsScissors);
+    
     // Handle each scenario
     if (vsScissors == "rock") {
         alert("You lose");
@@ -112,12 +156,18 @@ scissorsBtn.addEventListener('click', () => {
         alert("Draw");
         null;
     }
-
+    
     // Display Current Score
-    console.log("computer score: " + computerScore);
-    console.log("human score: " + humanScore);
+    // console.log("computer score: " + computerScore);
+    // console.log("human score: " + humanScore);
     scoreVal.textContent = `Computer Score: ${computerScore}. Your Score: ${humanScore}.`;
     if (computerScore < 5 && humanScore < 5) {
         return alert(`Computer Score: ${computerScore}. Your Score: ${humanScore}.`);
     } else { return winner();}
 });
+
+// Set up winner announcement... 
+const winnerDisplay = document.createElement('h2');
+winnerDisplay.setAttribute('id', 'winnerDisplay');
+winnerDisplay.textContent = "";
+body.appendChild(winnerDisplay);
